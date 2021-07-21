a3817bbfab5e8b78f8d4a2cb18bfc04367932679
adwait-da-succeedor <69492569+adwait-da-succeedor@users.noreply.github.com>
2021-07-20 19:35:05 -0500

    init

---
+++ cbe-applications/cbe-submissions/src/main/java/com/cbe/submissions/cbesubmissions/CbeSubmissionsApplication.java
@@ -0,0 +1,13 @@
+package com.cbe.submissions.cbesubmissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbeSubmissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbeSubmissionsApplication.class, args);
+	}
+
+}
---
+++ cbe-applications/cbe-submissions/src/test/java/com/cbe/submissions/cbesubmissions/CbeSubmissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbe.submissions.cbesubmissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbeSubmissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
